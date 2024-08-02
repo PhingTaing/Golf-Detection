10088e6094b242791d8ae7523b2e4b29729862bb
Phing Taing <43974014+PhingTaing@users.noreply.github.com>
2024-08-01 20:25:24 -0700

    Delete flask directory

--- flask/project/detection.py
+++
@@ -1,5 +0,0 @@
-class Detection:
-  def __init__(self, x, y, distance):
-    self.x = x
-    self.y = y
-    self.distance = distance
--- flask/venv/app.py
+++
@@ -1,8 +0,0 @@
-from flask import Flask
-
-app = Flask(name)
-
-
-@app.route('/')
-def hello():
-    return 'Hello, World!'
